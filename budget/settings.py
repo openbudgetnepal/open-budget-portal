b28a130222fca6c0354bee898cc321fcaab07c89
sijandh35 <sumoseason35@gmail.com>
2020-09-07 12:30:26 +0545

    setting

--- budget/settings.py
+++ budget/settings.py
@@ -79,10 +79,8 @@ WSGI_APPLICATION = 'budget.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nepalbudget1',
-        'USER': 'superuser',
-        "PASSWORD": '1234',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
